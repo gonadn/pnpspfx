8bf4d7ec37eb81f0e5d446edee1bdd6f507ee1aa
Gondal <adnan-riaz.gondal@capgemini.com>
2018-12-23 19:26:52 +0100

    tt

---
+++ pnpspfx/src/app/services/app.services.ts
@@ -0,0 +1,56 @@
+import { Injectable, OnInit } from '@angular/core';
+import pnp from "@pnp/pnpjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class BaseService implements OnInit {
+    private listName: string = 'ProgramMeny';
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    public getlistData() {
+        var promise = new Promise((resolve, reject) => {
+            if (this.listName !== null && this.listName !== undefined) {
+                pnp.sp.web.lists.getByTitle(this.listName).items.get().then((items: any[]) => {
+                    if (items !== null && items !== undefined) {
+                        resolve(items);
+                    }
+                    else {
+                        reject("Error")
+                    }
+                });
+            }
+        });
+        return promise;
+    }
+
+    public getlistData_() {
+        return pnp.sp.web.lists.getByTitle(this.listName).items.get().then((items: any[]) => {
+            if (items !== null && items !== undefined) {
+                return items;
+            }
+        });
+    }
+
+    public getById() {
+
+    }
+
+    public post() {
+
+    }
+
+    public update() {
+
+    }
+
+    public delete() {
+
+    }
+
+}
